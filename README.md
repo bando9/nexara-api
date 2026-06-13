1771ce7033c4f5ca38b45d898709c91ec66cc142
bando9 <bandomega1@gmail.com>
2026-06-13 12:39:41 +0700

    feat: implement Scalar docs Open API & update README

--- README.md
+++ README.md
@@ -1,11 +1,49 @@
-To install dependencies:
+# Nexara API
+
+## Getting Started
+
+Copy and edit `.env` file:
+
+```sh
+cp .env.example .env
+```
+
+Setup database:
+
+```sh
+# Run database only
+bun db:up
+```
+
+Install dependencies:
+
 ```sh
 bun install
 ```
 
-To run:
+Migrate database and generate Prisma Client:
+
 ```sh
-bun run dev
+bun db:migrate
+# prisma migrate dev
 ```
 
-open http://localhost:3000
+Seed initial products:
+
+```sh
+bun db:seed
+# prisma db seed
+```
+
+Run development server:
+
+```sh
+bun dev
+# bun run --hot src/index.ts
+```
+
+Open <http://localhost:3000>.
+
+## Production
+
+Make sure the `DATABASE_URL` is configured in `.env` file for usage with Docker Compose.
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
-import { Hono } from 'hono'
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { Scalar } from "@scalar/hono-api-reference";
+import { logger } from "hono/logger";
 
-const app = new Hono()
+const app = new OpenAPIHono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(logger());
 
-export default app
+app.openapi(
+  {
+    method: "get",
+    path: "/hello",
+    description: "get hello",
+    responses: {
+      200: {
+        description: "Successfully get hello",
+      },
+    },
+  },
+  (c) => {
+    const text = "Hello, Nexara here! :)";
+    return c.json(text, 200);
+  },
+);
+
+// API Docs
+app.doc("/openapi.json", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Nexara API",
+    description: "-",
+  },
+});
+
+app.get("/", Scalar({ url: "/openapi.json" }));
+
+export default app;
